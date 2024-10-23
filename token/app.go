d04c373d5eecd962d273a0ce334e6b397b0b2df7
rainhan <rainhan99@gmail.com>
2024-10-23 13:33:57 +0800

    add log

---
+++ token/app.go
@@ -0,0 +1,5 @@
+package token
+
+// project gotools
+// file token/appgo
+// Create by RainHan on 2024/06/09 22:33:40
---
+++ tools/error_log.go
@@ -0,0 +1,5 @@
+package tools
+
+// project gotools
+// file tools/logger.go
+// Create by RainHan on 2024/06/12 14:43:50
